fd4ecb6575fa10767de536a42c0c034fd59dd057
Craig Roche <cdxr01@gmail.com>
2013-11-13 15:18:24 -0500

    initial repository

---
+++ Test/QuickCheck/Shrink.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
+module Test.QuickCheck.Shrink where
+
+import Control.Applicative
+import Control.Monad
+import Control.Monad.Trans.Class
+import Control.Monad.Trans.Reader
+
+import Test.QuickCheck
+
+
+newtype Shrink r a = Shrink (ReaderT r [] a)
+    deriving (Functor, Applicative, Alternative, Monad, MonadPlus)
+
+runShrink :: Shrink r a -> r -> [a]
+runShrink (Shrink m) = drop 1 . runReaderT m
+
+shrinks :: (Arbitrary a) => (r -> a) -> Shrink r a
+shrinks = shrinksWith shrink
+
+shrinksWith :: (a -> [a]) -> (r -> a) -> Shrink r a
+shrinksWith sf rf = Shrink $ do
+    a <- asks rf
+    lift $ a : sf a
+
+shrink' :: (Arbitrary a) => a -> Shrink r a
+shrink' = shrinks . const
