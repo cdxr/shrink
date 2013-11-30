ed0656af1b7d6c51c54f94605f2843b490c172c3
Craig Roche <cdxr01@gmail.com>
2013-11-30 17:22:29 -0500

    major reimplementation of Shrink

--- Test/QuickCheck/Shrink.hs
+++ Test/QuickCheck/Shrink.hs
@@ -1,30 +1,28 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-
 module Test.QuickCheck.Shrink where
 
 import Control.Applicative
 import Control.Monad
-import Control.Monad.Trans.Class
-import Control.Monad.Trans.Reader
 
 import Test.QuickCheck
 
 
--- | @Shrink r a@ is a computation that shrinks @a@ as a component of
--- a larger type @r@ (typically a record).
-newtype Shrink r a = Shrink (ReaderT r [] a)
-    deriving (Functor, Applicative, Alternative, Monad, MonadPlus)
+data Shrink a = Shrink [a] a
+    deriving (Show, Eq, Ord)
 
-runShrink :: Shrink r a -> r -> [a]
-runShrink (Shrink m) = drop 1 . runReaderT m
+runShrink :: Shrink a -> [a]
+runShrink (Shrink xs _) = xs
 
-shrinks :: (Arbitrary a) => (r -> a) -> Shrink r a
-shrinks = shrinksWith shrink
+shrinks :: (a -> [a]) -> a -> Shrink a
+shrinks f x = Shrink (f x) x
 
-shrinksWith :: (a -> [a]) -> (r -> a) -> Shrink r a
-shrinksWith sf rf = Shrink $ do
-    a <- asks rf
-    lift $ a : sf a
+shrink' :: (Arbitrary a) => a -> Shrink a
+shrink' = shrinks shrink
 
-shrink' :: (Arbitrary a) => a -> Shrink r a
-shrink' = shrinks . const
+
+instance Functor Shrink where
+    fmap f (Shrink xs x) = Shrink (map f xs) (f x)
+
+instance Applicative Shrink where
+    pure = Shrink []
+    Shrink fs f <*> Shrink xs x =
+        Shrink (map ($ x) fs ++ map f xs) (f x)
